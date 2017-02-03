3e73ea0edb838898fcc55cad454194c3b0bf0e04
pyAddict <anshu.aditya@delhivery.com>
2017-02-03 14:44:04 +0530

    More Functionality Added

--- keras_all_code.py
+++ keras_all_code.py
@@ -1,19 +1,59 @@
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
+from keras.callbacks import Callback
 from keras.layers import Dense
 from keras.utils import np_utils
 from keras.wrappers.scikit_learn import KerasClassifier, KerasRegressor
-from sklearn.cross_validation import cross_val_score
+from sklearn.cross_validation import cross_val_score, cross_val_predict, KFold
 from sklearn.grid_search import GridSearchCV
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import StratifiedShuffleSplit
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
-
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix, mean_squared_error
+import matplotlib.pyplot as plt
 seed = 7
 np.random.seed(seed)
+
+class TrainingHistory(Callback):
+
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.predictions = []
+        self.i = 0
+        self.save_every = 50
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.i += 1        
+        if self.i % self.save_every == 0:        
+            pred = model.predict(x_train)
+            self.predictions.append(pred)
+            
+class Tools:
+
+    def __init__(self, *args):
+        pass
+
+    def _grid_search(self, x_train, y_train, model, params, cval=5):
+        kfold = KFold(
+            n=cval, random_state=seed)
+        grid = GridSearchCV(
+            estimator=model, param_grid=params, cv=kfold)
+        grid_result = grid.fit(
+            x_train, y_train)
+        print("Best: %f using %s" % (
+            grid_result.best_score_, grid_result.best_params_))
+        return grid_result.best_params_
+
+    def _cross_validation(self, x_train, y_train, extimator, cval=5, *args):
+        kfold = KFold(
+            n=cval, random_state=seed)
+        results = cross_val_score(
+            estimator, x_train, y_train, cv=kfold)
+        print("Baseline: %.2f%% (%.2f%%)" % (
+            results.mean()*100, results.std()*100))
 
 
 class Multiclass_Without_Grid_Search:
@@ -61,6 +101,8 @@ class Multiclass_Without_Grid_Search:
             print('Recall - ', recall_score(
                 y_test, y_pred, average="macro"))
 
+        model.fit(
+            self.x_train, self.y_train, nb_epoch=nb_epoch, batch_size=batch_size)
         yy_pred = model.predict(
             self.x_test)
         self.y_pred = np.argmax(
@@ -87,11 +129,12 @@ class Multiclass_Without_Grid_Search:
         gc.collect()
 
 
-class Binary_Without_Grid_Search:
+class Binary_Without_Grid_Search(TrainingHistory):
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.history = TrainingHistory()
 
     def _prepare_test_data(self, test_size=0.2, *arg):
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
@@ -139,6 +182,14 @@ class Binary_Without_Grid_Search:
             print('Recall - ', recall_score(
                 y_test, y_pred, average="binary"))
 
+        # Prediction
+        # will
+        # always be
+        # on
+        # complete
+        # data
+        model.fit(
+            self.x_train, self.y_train, nb_epoch=nb_epoch, batch_size=batch_size)
         yy_pred = model.predict(
             self.x_test)
         self.y_pred = [np.round(
@@ -155,6 +206,12 @@ class Binary_Without_Grid_Search:
         obj._plot_confusion_matrix()
         obj._plot_roc_curve(
             pos_label=1, predicted_prob=self.prob)
+        # plt.figure(figsize=(6, 3))
+        # plt.plot(self.history.losses)
+        # plt.ylabel('error')
+        # plt.xlabel('iteration')
+        # plt.title('training error')
+        # plt.show()
 
         import gc
         gc.collect()
@@ -205,7 +262,7 @@ class Binary_With_Grid_Search:
         param_grid = dict(
             optimizer=optimizers, nb_epoch=epochs, batch_size=batches, init=init)
         grid = GridSearchCV(
-            estimator=model, param_grid=param_grid)
+            estimator=model, param_grid=param_grid, cv=5)
         grid_result = grid.fit(
             self.x_train, self.y_train)
         print("Best: %f using %s" % (
@@ -238,9 +295,13 @@ class Binary_With_Grid_Search:
         # model
         model.fit(
             self.x_train, self.y_train, nb_epoch=grid_result.best_params_['nb_epoch'], batch_size=grid_result.best_params_['batch_size'])
-        self.y_pred = model.predict(
+        yy_pred = model.predict(
             self.x_test)
+        self.y_pred = [np.round(
+            x) for x in yy_pred]
         self.y_true = self.y_test
+        self.prob = model.predict_proba(
+            self.x_test)
         self._analyse_result()
 
     def _analyse_result(self, *arg):
@@ -267,7 +328,7 @@ class Regression_Without_Grid_Search:
 
     # define base
     # mode
-    def create_model():
+    def create_model(self):
         # create
         # model
         model = Sequential()
@@ -287,12 +348,102 @@ class Regression_Without_Grid_Search:
         estimators.append(
             ('standardize', StandardScaler()))
         estimators.append(('mlp', KerasRegressor(
-            build_fn=create_model, nb_epoch=50, batch_size=5, verbose=0)))
+            build_fn=self.create_model, nb_epoch=50, batch_size=5, verbose=1)))
         pipeline = Pipeline(
             estimators)
         kfold = KFold(
-            n=10, random_state=seed)
+            n=5, random_state=seed)
         results = cross_val_score(
-            pipeline, X, Y, cv=kfold)
+            pipeline, self.x_train, self.y_train, cv=kfold)
+        print(
+            results, '\n')
         print("Standardized: %.2f (%.2f) MSE" % (
-            results.mean(), results.std()))
+            results.mean(),
+            results.std()))
+
+        pipeline.fit(
+            self.x_train, self.y_train)
+        yy_pred = pipeline.predict(
+            self.x_test)
+        # yy_pred = cross_val_predict(
+        # pipeline,
+        # self.x_test,
+        # cv=kfold)
+        self.y_pred = yy_pred
+        self.y_true = self.y_test
+        self._analyse_result()
+
+    def _analyse_result(self, *arg):
+        print('RMSE on the test data \n', mean_squared_error(
+            self.y_true, self.y_pred)**0.5)
+
+
+class Regression_With_Grid_Search(Tools):
+
+    def __init__(self, x, y, *arg):
+        self.x = x
+        self.y = y
+
+    def _prepare_test_data(self, test_size=0.2, *arg):
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            self.x, self.y, test_size=test_size, random_state=seed)
+
+    # define base
+    # mode
+    def create_model(self, init='normal', optimizer='adam'):
+        # create
+        # model
+        model = Sequential()
+        model.add(Dense(
+            13, input_dim=13, init=init, activation='relu'))
+        model.add(
+            Dense(1, init=init))
+        # Compile
+        # model
+        model.compile(
+            loss='mean_squared_error', optimizer=optimizer)
+        return model
+
+    def _model_build(self, *arg):
+        self._prepare_test_data()
+        estimators = []
+        estimators.append(
+            ('standardize', StandardScaler()))
+        estimators.append(('mlp', KerasRegressor(
+            build_fn=self.create_model, nb_epoch=50, batch_size=5, verbose=0)))
+        pipeline = Pipeline(
+            estimators)
+        optimizers = [
+            'adam']
+        init = [
+            'normal', 'uniform']
+        epochs = [
+            100, 150]
+        batches = [
+            5, 10]
+        params = dict(mlp__optimizer=optimizers, mlp__nb_epoch=epochs,
+                      mlp__batch_size=batches, mlp__init=init)
+        best_params = Tools()._grid_search(
+            self.x_train, self.y_train, pipeline, params, 5)
+
+        # Creating
+        # best model
+        #Change init and optimezer of create model acc to bbest params
+        estimators = []
+        estimators.append(
+            ('standardize', StandardScaler()))
+        estimators.append(('mlp', KerasRegressor(
+            build_fn=self.create_model, nb_epoch=best_params['mlp__nb_epoch'],
+            batch_size=best_params['mlp__batch_size'], verbose=0)))
+        pipeline = Pipeline(
+            estimators)
+        pipeline.fit(
+            self.x_train, self.y_train)
+        self.y_pred = pipeline.predict(
+            self.x_test)
+        self.y_true = self.y_test
+        self._analyse_result()
+
+    def _analyse_result(self, *args):
+        print('RMSE on the test data \n', mean_squared_error(
+            self.y_true, self.y_pred)**0.5)
--- main.py
+++ main.py
@@ -1,36 +1,57 @@
-from keras_all_code import Multiclass_Without_Grid_Search,Binary_Without_Grid_Search,Binary_With_Grid_Search
+from keras_all_code import Multiclass_Without_Grid_Search,Binary_Without_Grid_Search,Binary_With_Grid_Search,Regression_Without_Grid_Search,Regression_With_Grid_Search
 import numpy as np
 import pandas as pd
 from keras.utils import np_utils
-#Multiclass Testing
-# from sklearn.preprocessing import LabelEncoder
-# df = pd.read_csv('./iris.data.csv', delimiter=',', header=None)
-# data = df.values
-# x = data[
-#     :, 0:4].astype(float)
-# y = data[:, 4]
+# Multiclass Testing
+from sklearn.preprocessing import LabelEncoder
+df = pd.read_csv('./iris.data.csv', delimiter=',', header=None)
+data = df.values
+x = data[
+    :, 0:4].astype(float)
+y = data[:, 4]
 
-# encoder = LabelEncoder()
-# encoder.fit(y)
-# encoded_y = encoder.transform(
-#     y)
-# dummy_y = np_utils.to_categorical(
-#     encoded_y)
-# obj = Multiclass(
-#     x, dummy_y)
-# obj._model_build()
+encoder = LabelEncoder()
+encoder.fit(y)
+encoded_y = encoder.transform(
+    y)
+dummy_y = np_utils.to_categorical(
+    encoded_y)
+obj = Multiclass(
+    x, dummy_y)
+obj._model_build()
 
-#Binary without grid search Testing
-# data = np.loadtxt('./pima-indians-diabetes.data.csv', delimiter=',')
-# x = data[:, 0:8]
-# y = data[:, 8]
-# obj = Binary_Without_Grid_Search(x,y)
-# obj._model_build()
+# Binary without grid search Testing
+data = np.loadtxt('./pima-indians-diabetes.data.csv', delimiter=',')
+x = data[:, 0:8]
+y = data[:, 8]
+obj = Binary_Without_Grid_Search(x,y)
+obj._model_build()
 
-#Binary with grid search Testing
+# Binary with grid search Testing
 data = np.loadtxt('./pima-indians-diabetes.data.csv', delimiter=',')
 x = data[:, 0:8]
 y = data[:, 8]
 obj = Binary_With_Grid_Search(x,y)
 obj._model_build()
 
+# Regression_without_Grid_Search
+load dataset
+df = pd.read_csv("./housing.data.csv", delim_whitespace=True, header=None)
+data = df.values
+# split into input (X) and output (Y) variables
+x = data[:,0:13]
+y = data[:,13]
+
+obj = Regression_Without_Grid_Search(x,y)
+obj._model_build()
+
+# Regression_with_Grid_Search
+load dataset
+df = pd.read_csv("./housing.data.csv", delim_whitespace=True, header=None)
+data = df.values
+# split into input (X) and output (Y) variables
+x = data[:,0:13]
+y = data[:,13]
+
+obj = Regression_With_Grid_Search(x,y)
+obj._model_build()
